7cba17081deff01c4aeb0ce3209e42d01ebd5543
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-03 16:35:54 +0300

    Added micropost length check

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :microposts
   root 'users#index'
 
+  resources :microposts
+  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
