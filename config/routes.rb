14be539c5ad3063d10d64b0a09e98e2f68b05744
MacBook Air 13 <ror.web_developer@yahoo.com>
2020-12-03 17:13:04 +0300

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :microposts
-  
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :microposts
+  end
+
 end
